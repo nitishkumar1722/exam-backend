c621a77c31ef6eab4e7978a922269b60a2c172c9
NITISH KUMAR <nitishkumar1792005@gmail.com>
2026-02-15 18:55:23 +0530

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -2,17 +2,21 @@ const express = require("express");
 const router = express.Router();
 const Student = require("../models/Student");
 
-// --- ADD STUDENT (GET) ---
+// ADD STUDENT (GET Method)
 router.get("/add", async (req, res) => {
     try {
-        const { name, rollNo, email, examId } = req.query;
-        
-        const newStudent = new Student({ name, rollNo, email, examId });
+        const { name, rollNo, examId } = req.query;
+
+        if (!name || !rollNo || !examId) {
+            return res.status(400).json({ msg: "Saari details bhariye bhai!" });
+        }
+
+        const newStudent = new Student({ name, rollNo, examId });
         await newStudent.save();
         
-        res.json({ message: "Student Added via GET!" });
+        res.json({ msg: `Student ${name} added successfully!` });
     } catch (err) {
-        res.status(500).json({ error: err.message });
+        res.status(500).json({ msg: "Error: " + err.message });
     }
 });
 
