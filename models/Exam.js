3ea8c3aecaf7a84bfbd2b4e08b82d6c9df9bc6c3
NITISH KUMAR <nitishkumar1792005@gmail.com>
2026-02-15 18:37:09 +0530

    Update Exam.js

--- models/Exam.js
+++ models/Exam.js
@@ -4,16 +4,8 @@ const ExamSchema = new mongoose.Schema({
     examTitle: { type: String, required: true },
     duration: { type: String, required: true },
     totalMarks: { type: String, required: true },
-    teacherEmail: { type: String, required: true }
-    questions: [
-        {
-            question: String,
-            options: [String],
-            answer: String
-        }
-    ]
+    teacherEmail: { type: String, required: true },
+    questions: { type: Array, default: [] } 
 });
 
-
 module.exports = mongoose.model("Exam", ExamSchema, "exams");
-
