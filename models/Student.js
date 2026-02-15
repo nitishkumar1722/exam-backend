f06e0af071f9ef07afe4835a53d11b89fca67ad2
NITISH KUMAR <nitishkumar1792005@gmail.com>
2026-02-15 18:56:07 +0530

    Update Student.js

--- models/Student.js
+++ models/Student.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 
 const StudentSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    rollNo: { type: String, required: true },
-    email: { type: String, required: true },
-    examId: { type: String, required: true }
+    rollNo: { type: String, required: true }, // Registration Number
+    examId: { type: String, required: true }  // Kis exam mein add ho raha hai
 });
 
 module.exports = mongoose.model("Student", StudentSchema, "students");
